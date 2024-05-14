af9eaec5b1eca9caaa20710f0f9bc79c3c871431
Abdul-Hadi-Waseem <shaikhahw54@gmail.com>
2024-05-14 21:04:18 +0500

    Test

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"shaikhahw54@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
 echo "\
@@ -11,3 +11,4 @@ headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
 " > ~/.streamlit/config.toml
+ 
